adac5b54a0936bcdbcb40638f25b7eaa4dbe6313
Manuel Mendoza <alejobit@gmail.com>
2018-05-10 16:07:29 -0500

    Adds configuration support for links and meta

--- config/json-api-illuminated.php
+++ config/json-api-illuminated.php
@@ -1,11 +1,20 @@
 <?php
 
 return [
+    // https://github.com/neomerx/json-api/wiki/Schemas
     'schemas' => [
     ],
+
     'encoder' => [
+        // https://github.com/neomerx/json-api/wiki#encoderoptions
         'options' => 0,
         'url_prefix' => null,
         'depth' => 512,
+
+        // http://jsonapi.org/format/#document-top-level
+        'links' => [
+        ],
+        'meta' => [
+        ],
     ],
 ];
