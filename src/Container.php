93862f40cb3466452c33e8226876e00cf8502627
Manuel Mendoza <alejobit@gmail.com>
2018-05-15 15:16:40 -0500

    Remove custom container method implementation

--- src/Container.php
+++ src/Container.php
@@ -2,16 +2,10 @@
 
 namespace Phpackage\Illuminated\JsonApi;
 
-use Illuminate\Support\Str;
-
 class Container extends \Neomerx\JsonApi\Schema\Container
 {
     protected function getResourceType($resource): string
     {
-        return str_replace(
-            '_',
-            '-',
-            Str::snake(Str::plural(class_basename($resource)))
-        );
+        return parent::getResourceType($resource);
     }
 }
